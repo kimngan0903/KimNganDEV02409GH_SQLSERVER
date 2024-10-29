9716a88f9b596f0e62892b63c36f0147d8abd855
kimngan0903 <ngan36758@gmail.com>
2024-10-29 11:39:48 +0700

    Ruby

---
+++ Sesson31_QuanLyHangHoa.sql
@@ -0,0 +1,37 @@
+﻿CREATE DATABASE Sesson31_QuanLyHangHoa
+GO
+
+USE Sesson31_QuanLyHangHoa
+GO
+
+--- Tạo bảng Category
+CREATE TABLE CATEGORY
+(
+	CategoryID bigint not null primary key IDENTITY(1,1)
+	,CategoryName nvarchar(500) not null
+	,Notes ntext
+	,Icon nvarchar(250)
+	,ParentID bigint not null
+	,Slug nvarchar(160) 
+	,Meta_Title nvarchar(100) not null
+	,Meta_Keyword nvarchar(500) not null
+	,Meta_Description nvarchar(500) not null
+	,Created_Date datetime default GETDATE()
+	,Updated_Date datetime default GETDATE()
+	,Created_By bigint default 'null'
+	,Updated_by bigint default 'null'
+	,IsDelete tinyint not null default '0'
+	,IsActive tinyint not null default '1'
+)
+GO
+
+--- Tạo bảng PRODUCT
+CREATE TABLE PRODUCT
+(
+	ProductID bigint not null primary key IDENTITY(2,1)
+	,ProductName nvarchar(500) not null
+	,Description ntext 
+	,Notes ntext
+	,Image nvarchar(550)
+)
+GO
